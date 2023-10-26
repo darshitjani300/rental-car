4d6978e0e4cfcaac618427af27eef7e708160592
darshit <janidarshit300@gmail.com>
2023-10-26 11:00:52 +0530

    Responsive navigation bar

--- src/Components/Home-Comp/NavBar.jsx
+++ src/Components/Home-Comp/NavBar.jsx
@@ -4,13 +4,13 @@ import Btn from "../../utils/Btn";
 const NavBar = () => {
   return (
     <nav className="flex justify-between items-center pt-3 md:my-5 px-5 p-container">
-      <div className="w-20 md:w-24 lg:w-28">
+      <div className="w-20 md:w-24 lg:w-28 xl:w-28">
         <a href="">
           <img src="/images/logo.png" alt="logo" />
         </a>
       </div>
       <div>
-        <ul className="md:gap-2 lg:gap-4 lg:text-[12px]  hidden text-[9px] md:text-[10px] md:flex font-semibold text-[#010103]">
+        <ul className="md:gap-2 lg:gap-4 hidden text-[9px] md:text-[10px] lg:text-[13px] xl:text-[1rem] md:flex xl:gap-10 font-semibold text-[#010103]">
           <li className="hover:text-[#ff4d30] cursor-pointer duration-200 ease-in">
             Home
           </li>
@@ -31,7 +31,7 @@ const NavBar = () => {
           </li>
         </ul>
       </div>
-      <div className="flex gap-2 justify-end text-[10px] md:text-[10px] lg:text-[12px] font-semibold text-[#010103]">
+      <div className="flex gap-4 justify-end text-[10px] md:text-[12px] lg:text-[14px] xl:[1rem] font-semibold text-[#010103]">
         <button className="hover:text-[#ff4d30] cursor-pointer duration-200 ease-in">
           Sign In
         </button>
